f2ee464d3740df0d53d684ff0897dbda6309b583
Fabrus98 <52037035+Fabrus98@users.noreply.github.com>
2020-12-05 15:24:43 +0100

    Update index.jsx

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Button } from '@chakra-ui/react';
 
 export function LandingPage( props ) {
-    const login = () => window.location.href = 'http://localhost:3001/api/auth/discord';
+    //const login = () => window.location.href = 'http://localhost:3001/api/auth/discord';
+    const login = () => window.location.href = domain + 'https://italianhubot-dashboard.herokuapp.com/api/auth/discord';
     return (
         <Button
             onClick={login}
