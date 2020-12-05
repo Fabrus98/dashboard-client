7c8371a2c3e4c5795eb0970eef74295d05c92062
Fabrus98 <52037035+Fabrus98@users.noreply.github.com>
2020-12-05 05:15:00 +0100

    Update index.jsx

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button } from '@chakra-ui/react';
 
 export function LandingPage( props ) {
-    const login = () => window.location.href = 'http://localhost:3001/api/auth/discord';
+    const login = () => window.location.href = 'http://localhost:3000/api/auth/discord';
     return (
         <Button
             onClick={login}
